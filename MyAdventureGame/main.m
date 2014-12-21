a000505bcad9da811b045b9898f437a8b2550bc6
jimtoepel <jim.toepel@gmail.com>
2014-12-20 16:20:20 -0800

    Initial Commit

---
+++ MyAdventureGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyAdventureGame
+//
+//  Created by Jim Toepel on 12/20/14.
+//  Copyright (c) 2014 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
